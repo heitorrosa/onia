fba60010d929101b2848eb0eabf07510e048a417
heitorrosa <76885858+heitorrosa@users.noreply.github.com>
2026-01-15 01:34:46 -0300

    .

--- 1.A/__main__.py
+++ 1.A/__main__.py
@@ -21,12 +21,31 @@ def loadDataset():
 def evaluateMA(TICKER):
     df = stocks.loc[f'{TICKER}'].copy()
 
-    df['MA 50'] = df['close'].rolling(window=50, min_periods=1).mean()
-    df['MA 200'] = df['close'].rolling(window=200, min_periods=1).mean()
+    df['MA 50'] = df['close'].rolling(window=50, min_periods=50).mean()
+    df['MA 200'] = df['close'].rolling(window=200, min_periods=200).mean()
+
+    df.to_csv(f'{TICKER}.csv')
 
     return df
 
 if __name__ == '__main__':
     stocks = loadDataset()
 
-    maNVDA = evaluateMA("NVDA")
+    stocksList = ['INTC']
+    for stock in stocksList:
+        try:
+            maData = evaluateMA(stock)
+        except Exception as e:
+            print(f'error: {e}')
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(maData['date'], maData['MA 50'], label='MA 50')
+        plt.plot(maData['date'], maData['MA 200'], label='MA 200')
+        plt.plot(maData['date'], maData['close'], label='Close Price')
+        plt.xlabel('Date')
+        plt.ylabel('Price ($)')
+        plt.legend()
+        plt.title(f'Moving Averages ({maData['date'].min().strftime('%Y-%m-%d')} - {maData['date'].max().strftime('%Y-%m-%d')}) {maData.iloc[0].name}')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(f'{maData.iloc[0].name}.png')
